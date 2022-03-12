23a3690aa8476242874b01571b2d468bf913805a
testteser <eleveneleven111z@gmail.com>
2022-03-12 17:20:39 +0500

    fixed

--- js/bookings.js
+++ js/bookings.js
@@ -1,13 +1,25 @@
-const bookingsTablesClose = document.querySelectorAll('.bookings-table__class-close');
-const bookingsTableContent = document.querySelector('.bookings-table__content');
-const bookingsItemMore = document.querySelectorAll('.bookings-table__item-more');
+const bookingsTablesClose = document.querySelectorAll('.bookings__class-close');
+const bookingsTableContent = document.querySelector('.bookings__content');
+const bookingsItemMore = document.querySelectorAll('.bookings__item-more');
 const bookingsItemClose = document.querySelectorAll('.bookings-more__close');
 const bookingsDeleteClose = document.querySelectorAll('.bookings-delete-close');
 const bookingsMore = document.querySelector('#bookings-more');
-const bookingsDelete= document.querySelector('#bookings-delete');
+const bookingsDelete = document.querySelector('#bookings-delete');
 const showMore = document.querySelector('#show-more');
+const bookingsDeleteShow = document.querySelector('#booking-delete-show');
+const bookingsDeleteCancel = document.querySelector('#bookings-delete-cancel');
 
 let counter = 0;
+
+bookingsDeleteShow.addEventListener('click', () => {
+    bookingsMore.classList.remove('show');
+    bookingsDelete.classList.add('show');
+})
+
+bookingsDeleteCancel.addEventListener('click', () => {
+    bookingsDelete.classList.remove('show');
+    bookingsMore.classList.add('show');
+})
 
 bookingsItemMore.forEach(item => {
     item.addEventListener('click', () => {
@@ -45,33 +57,33 @@ showMore.addEventListener('click', (e) => {
 
 function renderItem(button) {
     let skeleton = `
-            <div class="bookings-table__cell">
-                <div class="bookings-table__cell-info">
-                    <h2 class="bookings-table__cell-title">
+            <div class="bookings__cell">
+                <div class="bookings__cell-info">
+                    <h2 class="bookings__cell-title">
                         Бухара
                     </h2>
-                    <p class="bookings-table__cell-date">
+                    <p class="bookings__cell-date">
                         18 ноября. 2021 - 19 нояб. 2021
                     </p>
                 </div>
-                <div class="bookings-table__list">
-                    <div class="bookings-table__item">
-                        <img src="images/booking-hotel.png" alt="Фото отеля" class="bookings-table__item-img">
-                        <div class="bookings-table__item-content">
-                            <div class="bookings-table__item-info">
-                                <h3 class="bookings-table__item-name">
+                <div class="bookings__list">
+                    <div class="bookings__item">
+                        <img src="images/booking-hotel.png" alt="Фото отеля" class="bookings__item-img">
+                        <div class="bookings__item-content">
+                            <div class="bookings__item-info">
+                                <h3 class="bookings__item-name">
                                         DMG Apartments Hotel
                                     </h3>
-                                <p class="bookings-table__item-date">
+                                <p class="bookings__item-date">
                                         18 ноября. 2021 - 19 нояб. 2021
                                     </p>
-                                <span class="bookings-table__item-price">
+                                <span class="bookings__item-price">
                                         UZS 232 222
                                     </span>
-                                <span class="bookings-table__item-status status-is-cancelled">
+                                <span class="bookings__item-status status-is-cancelled">
                                         Отменено
                                     </span>
-                                <button class="bookings-table__item-more site-more">
+                                <button class="bookings__item-more site-more">
                                         <span></span>
                                     </button>
                             </div>
@@ -87,15 +99,3 @@ function renderItem(button) {
 
     if (counter >= 3) button.remove();
 }
-
-(function () {
-    $('#booking-delete-show').on('click', function () {
-        $('#bookings-more').removeClass('show');
-        $('#bookings-delete').addClass('show');
-    })
-
-    $('#bookings-delete-cancel').on('click', function () {
-        $('#bookings-delete').removeClass('show');
-        $('#bookings-more').addClass('show');
-    })
-})
